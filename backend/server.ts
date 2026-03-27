a06cf2eefe4df50104128ea49e9e339acff3d4b2
Night-Owl-10 <pranavramteke40@gmail.com>
2026-03-28 00:28:12 +0530

    Frontend url added to backend

--- backend/server.ts
+++ backend/server.ts
@@ -13,10 +13,23 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}));
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://multi-app-project.vercel.app",
+];
+
+app.use(
+    cors({
+        origin: function (origin, callback) {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
+        credentials: true,
+    })
+);
 
 app.use(express.json());
 app.use(cookieParser());
